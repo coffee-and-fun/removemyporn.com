fc6ce9e5ec27acbde81b73b0147c712ba19d3ed0
Robert James Gabriel <robert_gabriel@outlook.com>
2019-12-24 22:40:20 -0600

    bottom right

--- assets/js/site.js
+++ assets/js/site.js
@@ -35,10 +35,27 @@ function getUrlVars() {
     return vars;
 }
 
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-bottom-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
 
 var paramater = getUrlVars()["payment"];
 
-console.log(paramater);
+
 if (paramater === 'success') {
     toastr.success('You successfully paid. Thank you for your support.', 'Remove my P**n', {
         timeOut: 5000
