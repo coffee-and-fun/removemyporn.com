2dee95f79d3b2235aea8367a15b7ba220f00b8b7
Robert James Gabriel <robert_gabriel@outlook.com>
2021-01-01 05:52:15 +0000

    New release

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,65 +1,67 @@
-import { getUrlVars } from "./core/features.js";
-import "../css/override.css";
-import "../css/styles.css";
-import "../css/modal.css";
-import { Notyf } from "notyf";
-import "notyf/notyf.min.css"; // for React and Vue
+import { getUrlVars } from './core/features.js';
+import '../css/override.css';
+import '../css/styles.css';
+import '../css/modal.css';
+import { Notyf } from 'notyf';
+import 'notyf/notyf.min.css'; // for React and Vue
 const notyf = new Notyf();
-import "@fortawesome/fontawesome-free/js/all";
-import { loadStripe } from "@stripe/stripe-js";
-import "bootstrap/js/dist/modal";
+import '@fortawesome/fontawesome-free/js/all';
+import { loadStripe } from '@stripe/stripe-js';
+import 'bootstrap/js/dist/modal';
 
 async function load() {
-  let paramater = getUrlVars()["payment"];
-  if (paramater === "success") {
-    notyf.success("You successfully paid. Thank you for your support.");
-  }
-  if (paramater === "canceled") {
-    notyf.error("Your payment was canceled.");
-  }
-
-  const stripe = await loadStripe("pk_live_DG2IMIp7QYpSiuiTFvQI7ZFW00OEARkk0s");
-
-  let buyButton = document.getElementsByClassName("stripe-buy-button");
-
-  if (buyButton.length !== 0) {
-    for (var i = 0; i < buyButton.length; i++) {
-      buyButton[i].addEventListener("click", function () {
-        // When the customer clicks on the button, redirect
-        // them to Checkout.
-        notyf.success("Loading Stripe...");
-        stripe
-          .redirectToCheckout({
-            items: [
-              {
-                plan: "plan_GWiDO5WS3j8fNG",
-                quantity: 1,
-              },
-            ],
-            successUrl: "https://removemyporn.com?payment=success",
-            cancelUrl: "https://removemyporn.com/?payment=canceled",
-          })
-          .then(function (result) {
-            if (result.error) {
-              notyf.error(result.error.message);
-            }
-          });
-      });
+    let paramater = getUrlVars()['payment'];
+    if (paramater === 'success') {
+        notyf.success('You successfully paid. Thank you for your support.');
     }
-  }
+    if (paramater === 'canceled') {
+        notyf.error('Your payment was canceled.');
+    }
+
+    const stripe = await loadStripe(
+        'pk_live_DG2IMIp7QYpSiuiTFvQI7ZFW00OEARkk0s',
+    );
+
+    let buyButton = document.getElementsByClassName('stripe-buy-button');
+
+    if (buyButton.length !== 0) {
+        for (var i = 0; i < buyButton.length; i++) {
+            buyButton[i].addEventListener('click', function () {
+                // When the customer clicks on the button, redirect
+                // them to Checkout.
+                notyf.success('Loading Stripe...');
+                stripe
+                    .redirectToCheckout({
+                        items: [
+                            {
+                                plan: 'plan_GWiDO5WS3j8fNG',
+                                quantity: 1,
+                            },
+                        ],
+                        successUrl: 'https://removemyporn.com?payment=success',
+                        cancelUrl: 'https://removemyporn.com/?payment=canceled',
+                    })
+                    .then(function (result) {
+                        if (result.error) {
+                            notyf.error(result.error.message);
+                        }
+                    });
+            });
+        }
+    }
 }
 
 load();
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        //   console.log('SW registered: ', registration);
-      })
-      .catch((registrationError) => {
-        //    console.log('SW registration failed: ', registrationError);
-      });
-  });
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/service-worker.js')
+            .then((registration) => {
+                //   console.log('SW registered: ', registration);
+            })
+            .catch((registrationError) => {
+                //    console.log('SW registration failed: ', registrationError);
+            });
+    });
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,107 +1,110 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const WorkboxPlugin = require("workbox-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
-const SitemapPlugin = require("sitemap-webpack-plugin").default;
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
 const paths = [
-  "/terms",
-  "/welcome",
-  "/updates",
-  "/pricing",
-  "/privacy",
-  "/privacy",
+    '/terms',
+    '/welcome',
+    '/updates',
+    '/pricing',
+    '/privacy',
+    '/privacy',
 ];
 
 module.exports = {
-  mode: "production",
-  context: __dirname + "/app/",
-  entry: {
-    app: "./assets/js/index.js",
-  },
-  plugins: [
-    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      inject: false,
-      template: "./index.html",
-    }),
+    mode: 'production',
+    context: __dirname + '/app/',
+    entry: {
+        app: './assets/js/index.js',
+    },
+    plugins: [
+        // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: './index.html',
+        }),
 
-    new CopyPlugin([
-      { from: "assets/images", to: "assets/images/" },
-      { from: "assets/videos", to: "assets/videos/" },
-      { from: "assets/fonts", to: "assets/fonts/" },
-      { from: "CNAME", to: "" },
-      { from: "robots.txt", to: "" },
-      { from: "404.html", to: "" },
-      { from: "privacy.html", to: "" },
-      { from: "terms.html", to: "" },
-      { from: "updates.html", to: "" },
-      { from: "welcome.html", to: "" },
-      {
-        from: "../node_modules/@fortawesome/fontawesome-free/svgs/solid/",
-        to: "assets/svgs",
-      },
-      {
-        from: "../node_modules/@fortawesome/fontawesome-free/svgs/brands/",
-        to: "assets/svgs",
-      },
-      {
-        from: "../node_modules/@fortawesome/fontawesome-free/svgs/regular/",
-        to: "assets/svgs",
-      },
-    ]),
-    new MiniCssExtractPlugin(),
+        new CopyPlugin([
+            { from: 'assets/images', to: 'assets/images/' },
+            { from: 'assets/videos', to: 'assets/videos/' },
+            { from: 'assets/fonts', to: 'assets/fonts/' },
+            { from: 'CNAME', to: '' },
+            { from: 'robots.txt', to: '' },
+            { from: '404.html', to: '' },
+            { from: 'privacy.html', to: '' },
+            { from: 'terms.html', to: '' },
+            { from: 'updates.html', to: '' },
+            { from: 'welcome.html', to: '' },
+            {
+                from:
+                    '../node_modules/@fortawesome/fontawesome-free/svgs/solid/',
+                to: 'assets/svgs',
+            },
+            {
+                from:
+                    '../node_modules/@fortawesome/fontawesome-free/svgs/brands/',
+                to: 'assets/svgs',
+            },
+            {
+                from:
+                    '../node_modules/@fortawesome/fontawesome-free/svgs/regular/',
+                to: 'assets/svgs',
+            },
+        ]),
+        new MiniCssExtractPlugin(),
 
-    new SitemapPlugin("https://www.removemyporn.com", paths),
-    new WorkboxPlugin.GenerateSW({
-      // these options encourage the ServiceWorkers to get in there fast
-      // and not allow any straggling "old" SWs to hang around
-      importsDirectory: "assets/",
-      offlineGoogleAnalytics: true,
-      clientsClaim: true,
-      skipWaiting: true,
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(jpe?g|png|gif)$/,
-        loader: "url-loader",
-        options: {
-          // Images larger than 10 KB won’t be inlined
-          limit: 10 * 1024,
-        },
-      },
-      {
-        test: /\.svg$/,
-        loader: "svg-url-loader",
-        options: {
-          // Images larger than 10 KB won’t be inlined
-          limit: 10 * 1024,
-          // Remove quotes around the encoded URL –
-          // they’re rarely useful
-          noquotes: true,
-        },
-      },
-      {
-        test: /\.(png|jpe?g|gif|xml|ico|svg|webmanifest)$/i,
-        use: [
-          {
-            loader: "file-loader",
-          },
-        ],
-      },
+        new SitemapPlugin('https://www.removemyporn.com', paths),
+        new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            importsDirectory: 'assets/',
+            offlineGoogleAnalytics: true,
+            clientsClaim: true,
+            skipWaiting: true,
+        }),
     ],
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "docs"),
-  },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/,
+                loader: 'url-loader',
+                options: {
+                    // Images larger than 10 KB won’t be inlined
+                    limit: 10 * 1024,
+                },
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-url-loader',
+                options: {
+                    // Images larger than 10 KB won’t be inlined
+                    limit: 10 * 1024,
+                    // Remove quotes around the encoded URL –
+                    // they’re rarely useful
+                    noquotes: true,
+                },
+            },
+            {
+                test: /\.(png|jpe?g|gif|xml|ico|svg|webmanifest)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'docs'),
+    },
 };
